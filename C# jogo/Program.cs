111f86477fa495db9b487e0ee4974f2aed6198f2
GCSousa <guilherme camargo de sousa>
2023-07-25 18:12:40 -0300

    feat: fist commit

---
+++ C# jogo/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Bem vindo ao jogo da adivinhação");
+
+Random numAleatorio = new();
+int valorInteiro = numAleatorio.Next(0,100);
+int achou = 1;
+
+while(achou !=0){
+
+Console.WriteLine("escolha um numero de 0 a 100:\n");
+string resposta = Console.ReadLine();
+int respostaint = int.Parse(resposta);
+
+    if (respostaint == valorInteiro){        
+        Console.WriteLine("parabens, achou o numero!");
+        achou = 0;
+    }
+    else if (respostaint < valorInteiro){
+        Console.WriteLine("o numero é maior");
+    }else Console.WriteLine("o numero é menor");
+
+}
+
+Console.WriteLine(valorInteiro);
+Console.ReadLine();
